dc433c2ad6272ed7b5f68ac351e8b08b962d9328
Mark Lessard <nlessard@mon-avenir.ca>
2023-06-26 12:19:51 -0400

    TFinaleRobot - ICS3U

---
+++ tasks/t_finale_main.rb
@@ -0,0 +1,58 @@
+#!/opt/local/bin/ruby
+#Copyright 2012 Joseph Bergin
+#License: Creative Commons Attribution-Noncommercial-Share Alike 3.0 United States License
+#
+# 2023_06_01 - VERSION FINALE - Quiz 08 - Question 1
+# Fait par : Veronica Crnko 
+####################################################################
+
+$graphical = true
+
+require_relative "../karel/robota"
+require_relative "../karel/ur_robot"
+require_relative "../mixins/turner"
+require_relative '../mixins/sensor_pack'
+require_relative "t_finale_robot"
+ 
+# a task for putting beepers in the shape of a bell
+def task()
+ 
+  world = Robota::World
+  world.read_world("../karel/tache_finale_a.kwld")
+
+ akinorev = PickupBeepersSensorPack.new(1, 2, Robota::NORTH, 28)
+ akinorev.turn_left()
+ akinorev.move()
+ akinorev.turnleft_twice()
+ akinorev.move13_turnleft()
+ akinorev.move13_turnright()
+ akinorev.move13_turnleft()
+ akinorev.move13_turnright()
+ akinorev.move13_turnleft()
+ akinorev.move13_turnright()
+ akinorev.move13_turnleft()
+ akinorev.move13_turnright()
+ akinorev.move13_turnleft()
+ akinorev.move13_turnright()
+ akinorev.move13_turnleft()
+ akinorev.akino_movedown()
+ akinorev.put3_colonnes()
+ 
+end
+
+
+
+
+
+
+
+if __FILE__ == $0
+  if $graphical
+     screen = window(13, 100) # (size, speed)
+     screen.run do
+       task
+     end
+   else
+     task
+   end
+end
---
+++ tasks/t_finale_robot.rb
@@ -0,0 +1,116 @@
+#Copyright 2010 Joseph Bergin
+#License: Creative Commons Attribution-Noncommercial-Share Alike 3.0 United States License
+#
+# 2023_06_01 - VERSION FINALE - Quiz 08 - Question 1
+# Fait par : Veronica Crnko 
+####################################################################
+
+#!/opt/local/bin/ruby
+
+require_relative "../karel/ur_robot"
+require_relative "../mixins/turner"
+
+# A class whose robot can put beepers
+class PickupBeepersSensorPack < UrRobot
+include Turner
+
+def turnleft_twice
+	turn_left
+	turn_left
+end	
+
+def move13_turnleft
+	move
+	move
+	move
+	move
+	move
+	move
+	move
+	move
+	move
+	move
+	move
+	turn_left
+	move
+	turn_left
+end
+
+def move13_turnright
+	move
+    move
+	move
+	move
+	move
+	move
+	move
+	move
+	move
+	move
+	move
+	turn_right
+	move
+	turn_right
+end	
+
+def akino_movedown
+	turn_left
+	move
+	move
+	move
+	move
+	move
+	move
+	move
+	move
+	move
+	move
+	move
+    turn_right
+    move
+	move
+	move
+	move
+	move
+	move
+	move
+	move
+	move
+	move
+	move
+end
+ 
+ def put3_colonnes
+ 	 put_beeper
+ 	 turn_left
+ 	 turn_left
+ 	 move
+ 	 put_beeper
+ 	 turn_left
+ 	 move
+ 	 put_beeper
+     turn_right 
+     move 
+     put_beeper
+     turn_right
+     move
+     put_beeper
+     turn_left
+     turn_left
+     move
+     move
+     put_beeper
+ end    
+
+ end    
+
+
+
+
+end
+ 
+
+
+  
+
+
